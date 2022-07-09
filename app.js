188bccac5232b10754dd6d161f1ee436b5878699
Arthur Cruz <arthur.silva1041@gmail.com>
2022-07-09 00:01:23 -0300

    Sat 09 Jul 2022 12:01:23 AM -03

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
+const helperFunctions = {
+  appendHTML: (el, htmlCode) => {
+    el.insertAdjacentHTML('beforeend', htmlCode);
+  },
+};
+
 document.addEventListener('DOMContentLoaded', () => {
-  
   const productSection = {
     get functions() {
       return {
         activateGallery: () => {
+          const { appendHTML } = helperFunctions;
 
+          const currentImageEl = document.querySelector(
+            '.product-stuff-wrapper .media-wrapper .current-img img'
+          );
+          const thumbnailsEl = document.querySelectorAll(
+            '.product-stuff-wrapper .media-wrapper .thumbnails-wrapper .thumbnail'
+          );
+          const thumbnailsWrapperEl = document.querySelector(
+            '.product-stuff-wrapper .media-wrapper .thumbnails-wrapper'
+          );
+
+          const currentImageUrl = currentImageEl.src;
+
+          thumbnailsEl.forEach((el) => {
+            el.addEventListener('click', () => {
+              currentImageEl.src = el.querySelector("img").src;
+            });
+          });
         },
         displayDiscount: () => {
           if (!this.conditionals.shouldDisplayDiscount) {
@@ -23,8 +46,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
           qtySelector.addEventListener('input', (event) => {
             const enteredValue = event.data;
-            
-            if(enteredValue === null || enteredValue < 0 ) {
+
+            if (enteredValue === null || enteredValue < 0) {
               qtySelector.value = '1';
             } else if (parseInt(qtySelector.value) <= 0) {
               qtySelector.value = '1';
@@ -40,11 +63,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
   ((productSection) => {
     if (productSection) {
-      productSection.functions.displayDiscount();
-      
-      productSection.functions.activateGallery();
-      productSection.functions.qtyOnlyAllowPositiveNumbers();
+      const { displayDiscount, activateGallery, qtyOnlyAllowPositiveNumbers } =
+        productSection.functions;
+
+      // conditional functions
+      displayDiscount();
+
+      // functionalities
+      activateGallery();
+      qtyOnlyAllowPositiveNumbers();
     }
   })(productSection);
-
 });
